926487706c90c984f27e452da575f7487c692c40
khsagehorn <huntersagehorn@gmail.com>
2016-02-01 16:07:07 -0700

    api works

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,41 @@
 // add scripts
 
+window.myCallback = function (data) {
+  console.log('your response:', data);
+
+  $("#value").html(data.docSentiment.score);
+
+
+}
+
 $(document).on('ready', function() {
 
   $("#submit").on("click", function(event){
     event.preventDefault();
-    var text = $("#text-input").val();
+    var text = $("#text-input1").val();
+
     testText(text);
   });
 });
 
 function testText(text) {
+  var baseURL = "http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment";
+  var apiKey = "?apikey=abedd417c29793469d729bb648c5f7bcd56e583f";
+  var outputMode = "&outputMode=json&jsonp=myCallback";
+  var textURI = "&text=" + encodeURI(text);
  
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.aylien.com/api/v1/sentiment?mode=tweet&text=Look%20at%20this%20shit.%20Look%20at%20it.",
+    "url": baseURL + apiKey + outputMode + textURI,
+    // "url": "http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment?apikey=abedd417c29793469d729bb648c5f7bcd56e583f&outputMode=json&jsonp=myCallback&text=This%2520was%2520a%2520terrible%2520idea%252C%2520you%2520know.",
     "method": "GET",
-    "headers": {
-      "x-aylien-textapi-application-key": "ad58061c07dc1f78e7627809a9482ca8",
-      "x-aylien-textapi-application-id": "f725efbf",
-      "cache-control": "no-cache",
-      // "postman-token": "5a447de6-15a9-a741-a9e3-3f8d0b53a126"
-    }
-  }
+    "dataType": 'jsonp'
+}
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-  });
+  $.ajax(settings);
+
 };
+
+
 
